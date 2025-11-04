4adf57a0ec12a632878a416208b8f201da4f8dd0
Sebastian Asuncion Montero <desarrollo.devsam@gmail.com>
2025-11-04 22:44:39 +0100

    Create Header

--- biblioteca-code-cafe-frontend/src/pages/Home.tsx
+++ biblioteca-code-cafe-frontend/src/pages/Home.tsx
@@ -1,3 +1,13 @@
+import Header from "@/modules/core/components/Header";
+
 export const Home = () => {
-	return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <h1 className="text-center text-2xl font-bold mt-10">Bienvenido a Librería CodigoJava</h1>
+      {/* Aquí irán otros componentes */}
+      {/* <Footer /> */}
+    </div>
+  );
 };
+
